af044542097f5cca5256315657f48d19cd203e46
Aaron Lindsay <aaron@os.amperecomputing.com>
2026-01-29 20:58:09 -0700

    Add tutorial_03.hpp

---
+++ predictors/tutorial_03.hpp
@@ -0,0 +1,155 @@
+#include "../cbp.hpp"
+#include "../harcom.hpp"
+
+using namespace hcm;
+
+#define LINE_INSTRUCTIONS 16
+
+// Note: common.hpp contains a more generic version of a saturating counter
+// update, this is reproduced here for learning purposes.
+val<2> update_counter(val<2> counter, val<1> taken) {
+    val<2> increased = select(counter == 3, counter, val<2>{counter + 1});
+    val<2> decreased = select(counter == 0, counter, val<2>{counter - 1});
+    return select(taken, increased, decreased);
+}
+
+struct tutorial : predictor {
+    /*
+     * Predict up to 16 instructions per cycle using an SRAM array of 16
+     * two-bit counters.
+     */
+
+    ram<val<2 * LINE_INSTRUCTIONS>, 64> counters;
+
+    // Record values from prediction-time to be used at update-time
+    reg<6> index;
+    reg<2 * LINE_INSTRUCTIONS> counter;
+    arr<reg<1>, LINE_INSTRUCTIONS> saturated;
+    arr<reg<1>, LINE_INSTRUCTIONS> pred_taken;
+
+    // Record values from per-branch updates to be used in per-cacheline
+    // update logic
+    u64 num_branches = 0;
+    arr<reg<4>, LINE_INSTRUCTIONS> branch_offset;
+    arr<reg<1>, LINE_INSTRUCTIONS> branch_dir;
+
+    val<1> predict1([[maybe_unused]] val<64> inst_pc)
+    {
+        // Reset the count of the number of branches executed from this
+        // prediction block
+        num_branches = 0;
+
+        // Hash the instruction PC to a 6-bit index by first chunking it into
+        // an array of 6-bit values, then folding that array onto itself using
+        // XOR.
+        index = (inst_pc >> 6).make_array(val<6>{}).fold_xor();
+
+        // Index into the array of 2-bit counters, saving the counter value to
+        // a register
+        counter = counters.read(index);
+
+        // Save information about this prediction (its direction and whether
+        // the counter was saturated) to be used for other predictions this
+        // cycle and update-time.
+        arr<val<2>, LINE_INSTRUCTIONS> counter_arr = counter.make_array(val<2>{});
+        arr<val<1>, LINE_INSTRUCTIONS> saturated_val = [&](u64 i){ return (counter_arr[i] == hard<0>{}) | (counter_arr[i] == hard<3>{}); };
+        saturated = saturated_val;
+        arr<val<1>, LINE_INSTRUCTIONS> taken_val = [&](u64 i){ return counter_arr[i] >> 1;};
+        pred_taken = taken_val;
+
+        return predict(inst_pc);
+    };
+
+    val<1> predict(val<64> inst_pc)
+    {
+        // Use the top bit of the counter to predict the branch's direction
+        val<4> offset = inst_pc >> 2;
+        reuse_prediction(offset != (LINE_INSTRUCTIONS - 1));
+        return pred_taken.select(offset);
+    };
+
+    val<1> reuse_predict1([[maybe_unused]] val<64> inst_pc)
+    {
+        return predict(inst_pc);
+    };
+
+    val<1> reuse_predict2([[maybe_unused]] val<64> inst_pc)
+    {
+        return predict(inst_pc);
+    }
+
+    val<1> predict2([[maybe_unused]] val<64> inst_pc)
+    {
+        return predict(inst_pc);
+    }
+
+    void update_condbr([[maybe_unused]] val<64> branch_pc, [[maybe_unused]] val<1> taken, [[maybe_unused]] val<64> next_pc)
+    {
+        // For each conditional branch executed, record its offset and whether
+        // it was taken. This is used by `update_cycle` to perform updates at a
+        // cacheline granularity. Note that `num_branches` is a regular C++
+        // integer. Although most values are represented by HARCOM types, it is
+        // allowed to count branches and instructions using C++ integers and to
+        // use those counts as you see demonstrated here.
+        branch_offset[num_branches] = branch_pc >> 2;
+        branch_dir[num_branches] = taken;
+        num_branches++;
+    }
+
+    void update_cycle([[maybe_unused]] val<1> mispredict, [[maybe_unused]] val<64> next_pc)
+    {
+        // In order to perform updates for the current cacheline, we want a
+        // mask with one bit corresponding to one instruction offset in the
+        // line, but to start we have an array indexed by branch number. So we
+        // create one inner array per offset and fold that array into a single
+        // bit per offset.
+        arr<val<1>, LINE_INSTRUCTIONS> branch_arr = [&](u64 offset) {
+            arr<val<1>, LINE_INSTRUCTIONS> match_offset = [&](u64 i){
+                return (i < num_branches) & (branch_offset[i] == offset);
+            };
+            return match_offset.fold_or();
+        };
+        // Finally, we concatenate that array of 1-bit vals into a 16-bit val
+        val<LINE_INSTRUCTIONS> branch_mask = branch_arr.concat();
+
+        // The same logic as for `branch_arr` above, but this time we only want
+        // the bits in the mask set if this were a *taken* branch
+        arr<val<1>, LINE_INSTRUCTIONS> taken_arr = [&](u64 offset) {
+            arr<val<1>, LINE_INSTRUCTIONS> match_offset = [&](u64 i){
+                return (i < num_branches) & (branch_offset[i] == offset) & branch_dir[i];
+            };
+            return match_offset.fold_or();
+        };
+        val<LINE_INSTRUCTIONS> taken_mask = taken_arr.concat();
+
+        // Determine which offsets we want to update. We update any the counter
+        // of any offset which was a branch and that branch's counter was
+        // either incorrect or already-saturated.
+        val<LINE_INSTRUCTIONS> incorrect = taken_mask ^ pred_taken.concat();
+        val<LINE_INSTRUCTIONS> unsaturated_or_mispredicted_branch = branch_mask & (~saturated.concat() | incorrect);
+
+        // Determine whether to perform an update - when the updated counter is
+        // different than the read counter
+        val<1> performing_update = (unsaturated_or_mispredicted_branch != hard<0>{});
+
+        arr<val<1>, LINE_INSTRUCTIONS> should_update = unsaturated_or_mispredicted_branch.make_array(val<1>{});
+
+        arr<val<2>, LINE_INSTRUCTIONS> new_counters = [&](u64 i){
+            val<2> this_counter = counter.make_array(val<2>{})[i];
+            return select(should_update[i],
+                          update_counter(this_counter, taken_arr[i]),
+                          this_counter);
+        };
+
+        // If we are doing an update, inform the simulator we need an extra
+        // cycle to write the array (note this must be called *before* the
+        // array write below, or it will fail at runtime with a message like
+        // "single RAM access per cycle")
+        need_extra_cycle(performing_update);
+
+        // Finally, write back to the array (only if needed)
+        execute_if(performing_update, [&](){
+            counters.write(index, new_counters.concat());
+        });
+    }
+};
