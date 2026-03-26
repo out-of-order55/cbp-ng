382c676dc44573aec959d32af7a645cf37d2a491
out-of-order55 <515897968@qq.com>
2026-03-26 13:16:31 +0800

    Rollback RAM perf and debug counters

--- predictors/tage.hpp
+++ predictors/tage.hpp
@@ -3,6 +3,10 @@
 #define USE_META
 #define RESET_UBITS
 
+#ifndef MY_RWRAM_DEPTH
+#define MY_RWRAM_DEPTH 2
+#endif
+
 #include "../cbp.hpp"
 #include "../harcom.hpp"
 #include "common.hpp"
@@ -15,7 +19,7 @@
 
 using namespace hcm;
 
-template<u64 LOGLB=6, u64 NUMG=8, u64 LOGG=11, u64 LOGB=12, u64 TAGW=12, u64 GHIST=100, u64 LOGP1=14, u64 GHIST1=6>
+template<u64 LOGLB=4, u64 NUMG=8, u64 LOGG=11, u64 LOGB=12, u64 TAGW=11, u64 GHIST=100, u64 LOGP1=14, u64 GHIST1=6>
 struct tage : predictor {
     // provides 2^(LOGLB-2) predictions per cycle
     // P2 is a TAGE, P1 is a gshare
